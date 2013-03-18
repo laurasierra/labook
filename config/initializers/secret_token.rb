f469e5c69d69565dbe578e256877c65d2b2ddb0b
Elkin Sierra <mail@elkinsierra.com>
2013-03-17 23:46:58 -0400

    Create a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Labook::Application.config.secret_token = '57b106415fe0fe4353912731ed24a9b6b7299f49fb27737ea8e5d72d951d787657506e6c32cbc06b20ff2aa20389f043266461ef05ba3ed1dcef7ff10fbecb8f'
